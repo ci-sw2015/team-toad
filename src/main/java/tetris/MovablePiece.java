51ee12b47000e2fab858272fc506b8e93aff2bea
Team Rambo <team@rambo.com>
2015-12-15 14:29:29 +0100

    bugfix

--- src/main/java/tetris/MovablePiece.java
+++ src/main/java/tetris/MovablePiece.java
@@ -28,8 +28,7 @@ public class MovablePiece implements Grid {
 
     public boolean outsideBoard(int boardRows) {
         for (Point loc : Grids.allPointsOf(this)) {
-            if (piece.cellAt(loc) != EMPTY) {
-                if (asAbs(loc).row >= boardRows) {
+            if (piece.cellAt(loc) != EMPTY && asAbs(loc).row >= boardRows) {
                     return true;
                 }
             }
